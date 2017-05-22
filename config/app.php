78c58457b2c98269bc7a49942d950bee74c2e212
zhaiyu <zhaiyu_hb@hotmail.com>
2017-05-22 10:56:38 +0800

    add laravel-admin

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // laravel admin
+        Encore\Admin\Providers\AdminServiceProvider::class,
+
     ],
 
     /*
